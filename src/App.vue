1e78569cf2d4ebd3814277fa7b092623ba6fa496
Efaz Mohammed <44260523+WhiteHades@users.noreply.github.com>
2024-08-02 15:53:42 +0200

    removed default css

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="container">
-    <header>
-      <h1>Notes</h1>
-      <button>+</button>
-    </header>
-    <div class="cards-container">
-      <div class="card">
-        <p class="main-text"></p>
-        <p class="date"></p>
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes</h1>
+        <button>+</button>
+      </header>
+      <div class="cards-container">
+        <div class="card">
+          <p class="main-text"></p>
+          <p class="date"></p>
+        </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
+
+<style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+
+.container {
+  max-width: 1000px;
+  padding: 10px;
+  margin: 0 auto;
+}
+</style>
